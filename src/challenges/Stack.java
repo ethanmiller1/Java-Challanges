a8a0341f0d86c55d143fb1f2bbc8728945f749b0
Ethan Miller <ethan.romans5.8@gmail.com>
2019-08-06 11:15:20 -0500

    add stack constructor

---
+++ src/challenges/Stack.java
@@ -0,0 +1,26 @@
+package challenges;
+
+// What does this stack do?
+public class Stack {
+
+    // Hold the stack array.
+    private Object array[];
+
+    // Hold the top element in the array.
+    private Object head;
+
+    // Hold the size of the array.
+    private int capacity;
+
+    // Construct the stack array when a new stack is generated.
+    Stack(int capacity) {
+        // Constructor takes in the size of the array to be generated and creates an Object array of that size.
+        this.array = new Object[capacity];
+        // Set capacity equal to the size of the array.
+        this.capacity = capacity;
+        // We haven't hit the zeroth element yet until the first element is pushed into the array.
+        this.head = -1;
+    }
+
+
+}
